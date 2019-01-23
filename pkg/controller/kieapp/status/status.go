8bd756fdadab7683da6835593c68e4364a20acb4
Ruben Romero Montes <rromerom@redhat.com>
2019-01-23 15:14:39 +0100

    [KIECLOUD-64] Conditions to reflect current state
    Signed-off-by: Ruben Romero Montes <rromerom@redhat.com>

--- pkg/controller/kieapp/status/status.go
+++ pkg/controller/kieapp/status/status.go
@@ -10,43 +10,38 @@ import (
 
 var log = logs.GetLogger("kieapp.controller")
 
+// SetProvisioning - Sets the condition type to Provisioning and status True if not yet set.
+// Existing previous errors won't be removed until SetDeployed is invoked.
 func SetProvisioning(cr *v1.KieApp) bool {
 	log := log.With("kind", cr.Kind, "name", cr.Name, "namespace", cr.Namespace)
-	conditions := len(cr.Status.Conditions)
-	if conditions != 0 && cr.Status.Conditions[conditions-1].Type == v1.ProvisioningConditionType {
+	condIdx := getConditionIdx(cr, v1.ProvisioningConditionType)
+	if condIdx != -1 {
 		log.Debug("Status: unchanged status [provisioning].")
 		return false
 	}
+	setDeployed(cr, false)
 	log.Debug("Status: set provisioning")
 	condition := v1.Condition{
 		Type:               v1.ProvisioningConditionType,
 		Status:             corev1.ConditionTrue,
 		LastTransitionTime: metav1.Now(),
 	}
-	cr.Status.Conditions = append(cr.Status.Conditions, condition)
+	if condIdx == -1 {
+		cr.Status.Conditions = append(cr.Status.Conditions, condition)
+	}
 	return true
 }
 
+// SetDeployed - Updates (if changed) the condition with the DeployedCondition and True status
 func SetDeployed(cr *v1.KieApp) bool {
-	log := log.With("kind", cr.Kind, "name", cr.Name, "namespace", cr.Namespace)
-	conditions := len(cr.Status.Conditions)
-	if conditions != 0 && cr.Status.Conditions[conditions-1].Type == v1.DeployedConditionType {
-		log.Debug("Status: unchanged status [deployed].")
-		return false
-	}
-	log.Debug("Status: set deployed")
-	condition := v1.Condition{
-		Type:               v1.DeployedConditionType,
-		Status:             corev1.ConditionTrue,
-		LastTransitionTime: metav1.Now(),
-	}
-	cr.Status.Conditions = append(cr.Status.Conditions, condition)
-	return true
+	return setDeployed(cr, true)
 }
 
+// SetFailed - Sets the failed condition to the status
 func SetFailed(cr *v1.KieApp, reason v1.ReasonType, err error) {
 	log := log.With("kind", cr.Kind, "name", cr.Name, "namespace", cr.Namespace)
 	log.Debug("Status: set failed")
+	setDeployed(cr, false)
 	condition := v1.Condition{
 		Type:               v1.FailedConditionType,
 		Status:             corev1.ConditionTrue,
@@ -54,11 +49,57 @@ func SetFailed(cr *v1.KieApp, reason v1.ReasonType, err error) {
 		Reason:             reason,
 		Message:            err.Error(),
 	}
-	cr.Status.Conditions = append(cr.Status.Conditions, condition)
+	condIdx := getConditionIdx(cr, v1.FailedConditionType)
+	if condIdx == -1 {
+		cr.Status.Conditions = append(cr.Status.Conditions, condition)
+	} else {
+		cr.Status.Conditions[condIdx] = condition
+	}
 }
 
+// SetDeployments - sets the deployment names to the status
 func SetDeployments(cr *v1.KieApp, deployments []string) {
 	log := log.With("kind", cr.Kind, "name", cr.Name, "namespace", cr.Namespace)
 	log.Debug("Status: update deployments")
 	cr.Status.Deployments = deployments
+}
+
+// setDeployed Adds the DeployedCondition if it doesn't exist or replaces it by
+// the previous one only if the status is different
+func setDeployed(cr *v1.KieApp, isDeployed bool) bool {
+	log := log.With("kind", cr.Kind, "name", cr.Name, "namespace", cr.Namespace)
+	status := corev1.ConditionFalse
+	if isDeployed {
+		status = corev1.ConditionTrue
+	}
+	condIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	if condIdx != -1 && cr.Status.Conditions[condIdx].Status == status {
+		log.Debugf("Status: unchanged status [deployed:%v].", status)
+		return false
+	}
+	log.Debugf("Status: changed status [deployed:%v].", status)
+
+	condition := v1.Condition{
+		Type:               v1.DeployedConditionType,
+		Status:             status,
+		LastTransitionTime: metav1.Now(),
+	}
+	if isDeployed {
+		cr.Status.Conditions = cr.Status.Conditions[:0]
+	}
+	if isDeployed || condIdx == -1 {
+		cr.Status.Conditions = append(cr.Status.Conditions, condition)
+	} else {
+		cr.Status.Conditions[condIdx] = condition
+	}
+	return true
+}
+
+func getConditionIdx(cr *v1.KieApp, condType v1.ConditionType) int {
+	for i, condition := range cr.Status.Conditions {
+		if condition.Type == condType {
+			return i
+		}
+	}
+	return -1
 }
--- pkg/controller/kieapp/status/status_test.go
+++ pkg/controller/kieapp/status/status_test.go
@@ -1,6 +1,7 @@
 package status
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/kiegroup/kie-cloud-operator/pkg/apis/app/v1"
@@ -39,9 +40,17 @@ func TestSetProvisioning(t *testing.T) {
 	assert.True(t, SetProvisioning(cr))
 
 	assert.NotEmpty(t, cr.Status.Conditions)
-	assert.Equal(t, v1.ProvisioningConditionType, cr.Status.Conditions[0].Type)
-	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[0].Status)
-	assert.True(t, now.Before(&cr.Status.Conditions[0].LastTransitionTime))
+	assert.Equal(t, 2, len(cr.Status.Conditions))
+
+	provIdx := getConditionIdx(cr, v1.ProvisioningConditionType)
+	assert.NotEqual(t, -1, provIdx)
+	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[provIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[provIdx].LastTransitionTime))
+
+	deployedIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionFalse, cr.Status.Conditions[deployedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[deployedIdx].LastTransitionTime))
 }
 
 func TestSetProvisioningSkipUpdate(t *testing.T) {
@@ -52,8 +61,12 @@ func TestSetProvisioningSkipUpdate(t *testing.T) {
 	condition := cr.Status.Conditions[0]
 
 	assert.False(t, SetProvisioning(cr))
-	assert.Equal(t, 1, len(cr.Status.Conditions))
+	assert.Equal(t, 2, len(cr.Status.Conditions))
 	assert.Equal(t, condition, cr.Status.Conditions[0])
+
+	deployedIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionFalse, cr.Status.Conditions[deployedIdx].Status)
 }
 
 func TestSetProvisioningAndThenDeployed(t *testing.T) {
@@ -65,12 +78,77 @@ func TestSetProvisioningAndThenDeployed(t *testing.T) {
 
 	assert.NotEmpty(t, cr.Status.Conditions)
 	condition := cr.Status.Conditions[0]
-	assert.Equal(t, 2, len(cr.Status.Conditions))
-	assert.Equal(t, v1.ProvisioningConditionType, condition.Type)
-	assert.Equal(t, corev1.ConditionTrue, condition.Status)
+	assert.Equal(t, 1, len(cr.Status.Conditions))
 	assert.True(t, now.Before(&condition.LastTransitionTime))
+	assert.Equal(t, v1.DeployedConditionType, condition.Type)
+	assert.Equal(t, corev1.ConditionTrue, condition.Status)
+}
 
-	assert.Equal(t, v1.DeployedConditionType, cr.Status.Conditions[1].Type)
-	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[1].Status)
-	assert.True(t, condition.LastTransitionTime.Before(&cr.Status.Conditions[1].LastTransitionTime))
+func TestSetProvisioningAndThenFailed(t *testing.T) {
+	now := metav1.Now()
+	cr := &v1.KieApp{}
+
+	assert.True(t, SetProvisioning(cr))
+	SetFailed(cr, v1.DeploymentFailedReason, errors.New("Test"))
+
+	assert.NotEmpty(t, cr.Status.Conditions)
+	assert.Equal(t, 3, len(cr.Status.Conditions))
+	provIdx := getConditionIdx(cr, v1.ProvisioningConditionType)
+	assert.NotEqual(t, -1, provIdx)
+	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[provIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[provIdx].LastTransitionTime))
+
+	deployedIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionFalse, cr.Status.Conditions[deployedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[deployedIdx].LastTransitionTime))
+
+	failedIdx := getConditionIdx(cr, v1.FailedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[failedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[failedIdx].LastTransitionTime))
+	assert.Equal(t, v1.DeploymentFailedReason, cr.Status.Conditions[failedIdx].Reason)
+	assert.Equal(t, "Test", cr.Status.Conditions[failedIdx].Message)
+}
+
+func TestSetFailedAndAgainFailed(t *testing.T) {
+	now := metav1.Now()
+	cr := &v1.KieApp{}
+
+	SetFailed(cr, v1.DeploymentFailedReason, errors.New("Test 1"))
+	SetFailed(cr, v1.DeploymentFailedReason, errors.New("Test 2"))
+
+	assert.NotEmpty(t, cr.Status.Conditions)
+	assert.Equal(t, 2, len(cr.Status.Conditions))
+
+	deployedIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionFalse, cr.Status.Conditions[deployedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[deployedIdx].LastTransitionTime))
+
+	failedIdx := getConditionIdx(cr, v1.FailedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[failedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[failedIdx].LastTransitionTime))
+	assert.Equal(t, v1.DeploymentFailedReason, cr.Status.Conditions[failedIdx].Reason)
+	assert.Equal(t, "Test 2", cr.Status.Conditions[failedIdx].Message)
+}
+
+func TestSetDeployedAndThenProvisioning(t *testing.T) {
+	now := metav1.Now()
+	cr := &v1.KieApp{}
+
+	assert.True(t, SetDeployed(cr))
+	assert.True(t, SetProvisioning(cr))
+
+	assert.NotEmpty(t, cr.Status.Conditions)
+	provIdx := getConditionIdx(cr, v1.ProvisioningConditionType)
+	assert.NotEqual(t, -1, provIdx)
+	assert.Equal(t, corev1.ConditionTrue, cr.Status.Conditions[provIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[provIdx].LastTransitionTime))
+
+	deployedIdx := getConditionIdx(cr, v1.DeployedConditionType)
+	assert.NotEqual(t, -1, deployedIdx)
+	assert.Equal(t, corev1.ConditionFalse, cr.Status.Conditions[deployedIdx].Status)
+	assert.True(t, now.Before(&cr.Status.Conditions[deployedIdx].LastTransitionTime))
 }
